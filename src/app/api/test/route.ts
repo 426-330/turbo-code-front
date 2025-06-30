f683b625ff7cdebac4792f80d7b13e6d8f6fbd17
Brian Ma <426-330@163.com>
2025-06-30 20:04:49 +0800

    更新 tsconfig.json 文件，禁用严格模式；重构 API 路由，移除不必要的请求参数，简化 GET 方法实现。

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,8 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { getCloudflareContext } from "@opennextjs/cloudflare";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const userId = 1;
+
+  // @ts-expect-error - DB is not defined in the context
   const db = getCloudflareContext().env.DB;
   const stmt = await db.prepare(`SELECT * FROM user WHERE id = ?`).bind(userId);
   const returnValue = await stmt.first();
